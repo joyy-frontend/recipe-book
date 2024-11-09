ac2bba92856e2053cef8ea051041b998ac78ef59
minimanimox <purplemushroomsoup@gmail.com>
2024-11-08 20:27:09 -0800

    feat: made logo in navbar

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,37 +1,62 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "../Custom.css";
-
+import logo from "../assets/images/logo.png";
 export default function Navbar({ isLoggedIn, onLogout }) {
   return (
     <nav className="navbar navbar-expand-sm navbar-light">
       <div className="container">
-          <ul className="navbar-nav me-auto mt-2 mt-lg-0">
-          <li className="nav-item">
-                    <Link className="nav-link" to="/">Home</Link>
+        {/* 로고 */}
+        <Link className="navbar-brand" to="/">
+          <img src={logo} alt="logo" className="nav-logo" />
+        </Link>
+
+        {/* 햄버거 버튼 */}
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* 네비게이션 메뉴 */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto align-items-center">  {/* ms-auto로 오른쪽 정렬 */}
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/recipes">
+                Recipes
+              </Link>
+            </li>
+            {isLoggedIn ? (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/mypage">
+                    My Page
+                  </Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="/recipes">Recipes</Link>
+                  <button className="btn btn-link nav-link" onClick={onLogout}>
+                    Logout
+                  </button>
                 </li>
-                {isLoggedIn ? (
-                    <>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/mypage">My Page</Link>
-                        </li>
-                        <li className="nav-item">
-                            <button className="btn btn-link nav-link" onClick={onLogout}>Logout</button>
-                        </li>
-                    </>
-                ) : (
-                    <>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/login">Login</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/register">Register</Link>
-                        </li>
-                    </>
-                )}
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/register">
+                    Register
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
+        </div>
       </div>
     </nav>
   );
