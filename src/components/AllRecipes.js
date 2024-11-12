9a6ee44ecba7812623660c3093cd20b6d8614beb
rhkk7157 <rhkk7157@gmail.com>
2024-11-12 02:32:05 -0800

    update nodata of recipe List at recipes page

--- src/components/AllRecipes.js
+++ src/components/AllRecipes.js
@@ -4,7 +4,6 @@ import pizza from "../assets/images/pizza.png";
 import logoImage from "../assets/images/logo.png";
 
 const AllRecipes = ({category, PropsRecipes}) => {
-  console.log(PropsRecipes)
   const [recipes, setRecipes] = useState([]);
 
   // useEffect(() => {
@@ -27,36 +26,39 @@ const filteredRecipes = category === "All"
     <div className="container py-5">
       <h2 className="text-center mb-4">All Recipes</h2>
       <div className="row g-4">
-        {PropsRecipes.map((recipe) => (
-          <div className="col-md-4" key={recipe.id}>
-            <div className="recipe-card">
-              <Link to={`/recipes/${recipe.id}`}>
-                {/* <img
-                  src={recipe.image
-                    ? require(`../assets/images/${recipe.image}`)
-                    : logoImage}
-                  alt={recipe.title}
-                  style={{ maxWidth: "250px" }}
-                /> */}
-                <img
-                  src={recipe.image}
-                  alt={recipe.title}
-                  style={{ maxWidth: "250px" }}
-                />
-                <div>
-                  <h3 className="card-title">{recipe.title}</h3>
-                  <p className="card-text">Recipe by {recipe.user}</p>
-                  <p className="card-text">
-                    <strong>Category:</strong> {recipe.category}
-                  </p>
-                  <p className="card-text">
-                    <strong>Likes:</strong> {recipe.likes}
-                  </p>
+        {
+          PropsRecipes ?
+            PropsRecipes.map((recipe) => (
+              <div className="col-md-4" key={recipe.id}>
+                <div className="recipe-card">
+                  <Link to={`/recipes/${recipe.id}`}>
+                    {/* <img
+                      src={recipe.image
+                        ? require(`../assets/images/${recipe.image}`)
+                        : logoImage}
+                      alt={recipe.title}
+                      style={{ maxWidth: "250px" }}
+                    /> */}
+                    <img
+                      src={recipe.image}
+                      alt={recipe.title}
+                      style={{ maxWidth: "250px" }}
+                    />
+                    <div>
+                      <h3 className="card-title">{recipe.title}</h3>
+                      <p className="card-text">Recipe by {recipe.user}</p>
+                      <p className="card-text">
+                        <strong>Category:</strong> {recipe.category}
+                      </p>
+                      <p className="card-text">
+                        <strong>Likes:</strong> {recipe.likes}
+                      </p>
+                    </div>
+                  </Link>
                 </div>
-              </Link>
-            </div>
-          </div>
-        ))}
+              </div>
+          )) : <div className="text-center mt-6">There is no Data</div>
+        }
 
         {/* <div className="col-md-4">
           <div className="recipe-card">
