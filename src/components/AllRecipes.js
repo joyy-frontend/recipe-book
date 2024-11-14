8ca73aed50fce2a5fa6b64af69b1ae323bfbbad5
Yunzoo01 <joyunzoooo@gmail.com>
2024-11-14 00:26:51 -0800

    Recipe CRUD + Category Filtereing , Searching

--- src/components/AllRecipes.js
+++ src/components/AllRecipes.js
@@ -1,82 +1,99 @@
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import logoImage from "../assets/images/logo.png";
 
-const AllRecipes = ({category, PropsRecipes}) => {
-  const navigate = useNavigate();
-
-  const onClickDel = (recipeId) => {
-    let storageData = JSON.parse(localStorage.getItem('recipe')) || [];
-    const updatedData = storageData.filter(item => item.id !== parseInt(recipeId));
-
-    localStorage.setItem("recipe", JSON.stringify(updatedData));
-
-    alert("Recipe deleted successfully");
-    navigate(0);
-  }
 const AllRecipes = ({ category, PropsRecipes, searchWord }) => {
   const [recipes, setRecipes] = useState(PropsRecipes);
+  const navigate = useNavigate();
 
   useEffect(() => {
     setRecipes(PropsRecipes);
   }, [PropsRecipes]);
 
+  const onClickDel = (recipeId) => {
+    let storageData = JSON.parse(localStorage.getItem("recipe")) || [];
+    const updatedData = storageData.filter(
+      (item) => item.id !== parseInt(recipeId)
+    );
+
+    localStorage.setItem("recipe", JSON.stringify(updatedData));
+    alert("Recipe deleted successfully");
+    
+    setRecipes(updatedData);
+    navigate(0);
+  };
+
   const filteredRecipes = recipes.filter((recipe) => {
-
-    const resultCategory = 
-      category.toLowerCase().trim() === "all" || 
-      recipe.category.toLowerCase().trim() === category.toLowerCase().trim();
-
+    // category가 배열이 아니면 배열로 바꿔야됨 --> map돌릴거라서
+    const arrayCategories = Array.isArray(category)
+      ? category.map((cate) => cate.toLowerCase().trim())
+      : [category.toLowerCase().trim()];
+  
+    const recipeCategories = recipe.category
+      .toLowerCase()
+      .split(",")
+      .map((cate) => cate.trim()); // ,(컴마) 기준으로 자름. "salad, appetizer" -> ["salad", "appetizer"]
+    console.log(recipeCategories);
+  
+    const resultCategory =
+    arrayCategories.includes("all") ||
+      recipeCategories.some((cate) => arrayCategories.includes(cate));
+  
     const resultSearch =
       searchWord === "" ||
-      recipe.title.toLowerCase().includes(searchWord.toLowerCase()) ||
-      recipe.user.toLowerCase().includes(searchWord.toLowerCase()) ||
-      recipe.content.toLowerCase().includes(searchWord.toLowerCase()) ||
-      recipe.category.toLowerCase().includes(searchWord.toLowerCase());
-
-      console.log(resultCategory, resultSearch); // T T, T F, F F 이런식으로 찍힘
-      
+      (recipe.title &&
+        recipe.title.toLowerCase().includes(searchWord.toLowerCase().trim())) ||
+      (recipe.user &&
+        recipe.user.toLowerCase().includes(searchWord.toLowerCase().trim())) ||
+      (recipe.content &&
+        recipe.content.toLowerCase().includes(searchWord.toLowerCase().trim())) ||
+      (recipe.category &&
+        recipe.category.toLowerCase().includes(searchWord.toLowerCase().trim()));
+  
     return resultCategory && resultSearch;
-    
   });
+  
   
 
   return (
     <div className="container py-5">
       <h2 className="text-center mb-4">All Recipes</h2>
       <div className="row g-4">
-        {
-          PropsRecipes ?
-            PropsRecipes.map((recipe) => (
-              <div className="col-md-4" key={recipe.id}>
-                <div className="recipe-card">
-                  <Link to={`/recipes/${recipe.id}`}>
-                    <img
-                      src={recipe.image}
-                      alt={recipe.title}
-                      style={{ width: '250px', height: '250px' }}
-                    />
-                    <div>
-                      <h3 className="card-title">{recipe.title}</h3>
-                      <p className="card-text">Recipe by {recipe.user}</p>
-                      <p className="card-text">
-                        <strong>Category:</strong> {recipe.category}
-                      </p>
-                      <p className="card-text">
-                        <strong>Likes:</strong> {recipe.likes}
-                      </p>
-                    </div>
-                  </Link>
-                  <button
-                    type="button"
-                    class="btn btn-outline-primary"
-                    style={{ width: '40%', textAlign:'center' }}
-                    onClick={()=> onClickDel(recipe.id)}
-                  >
-                    Delete
-                  </button>
-                </div>
+        {filteredRecipes.length > 0 ? (
+          filteredRecipes.map((recipe) => (
+            <div className="col-md-4" key={recipe.id}>
+              <div className="recipe-card">
+                <Link to={`/recipes/${recipe.id}`}>
+                  <img
+                    src={recipe.image ? recipe.image : logoImage}
+                    alt={recipe.title}
+                    style={{ width: "250px", height: "250px" }}
+                  />
+                  <div>
+                    <h3 className="card-title">{recipe.title}</h3>
+                    <p className="card-text">Recipe by {recipe.user}</p>
+                    <p className="card-text">
+                      <strong>Category:</strong> {recipe.category}
+                    </p>
+                    <p className="card-text">
+                      <strong>Likes:</strong> {recipe.likes}
+                    </p>
+                  </div>
+                </Link>
+                <button
+                  type="button"
+                  className="btn btn-outline-primary"
+                  style={{ width: "40%", textAlign: "center" }}
+                  onClick={() => onClickDel(recipe.id)}
+                >
+                  Delete
+                </button>
               </div>
-          )) : <div className="text-center mt-6">There is no Data</div>
-        }
+            </div>
+          ))
+        ) : (
+          <div className="text-center mt-6">There is no Data</div>
+        )}
       </div>
     </div>
   );
--- src/pages/RecipeList.js
+++ src/pages/RecipeList.js
@@ -4,45 +4,43 @@ import { useEffect, useState } from "react";
 
 export default function RecipeList() {
   const [recipes, setRecipes] = useState([]);
-  const navigate = useNavigate();
   const [category, setCategory] = useState("All");
   const [searchWord, setSearchWord] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const localStoragedData = JSON.parse(localStorage.getItem('recipe'));
-    
-    // 데이터가 배열이 아닌 경우 배열로 감싸서 설정함. --> filter()할때 배열로 받아와야되어서..
+    const localStoragedData = JSON.parse(localStorage.getItem("recipe"));
+
+   // 데이터가 배열이 아닌 경우 배열로 감싸서 설정함. --> filter()할때 배열로 받아와야되어서..
     if (localStoragedData) {
       const processedData = Array.isArray(localStoragedData)
-      ? localStoragedData
-      : [localStoragedData];
-    // localstorage 객체를 불러올때 순차적으로 id를 추가해서 저장함--> 안 추가하면 map돌릴때 warning 떠서 가져올때 id도 같이 배열에 저장함.
-    const dataWithId = processedData.map((recipe, index) => ({
-      ...recipe,
-      id: recipe.id || index + 1, 
-    }));
+        ? localStoragedData
+        : [localStoragedData];
 
-    setRecipes(dataWithId);
-  } else {
-    setRecipes([]); // localStorage에 데이터가 없으면 빈 배열로 설정함.
-  }
-}, []);
+       // localstorage 객체를 불러올때 순차적으로 id를 추가해서 저장함--> 안 추가하면 map돌릴때 warning 떠서 가져올때 id도 같이 배열에 저장함.
+      const dataWithId = processedData.map((recipe, index) => ({
+        ...recipe,
+        id: recipe.id || index + 1,
+      }));
+
+      setRecipes(dataWithId);
+    } else {
+      setRecipes([]); // localStorage에 데이터가 없으면 빈 배열로 설정함. 
+    }
+  }, []);
 
   const handleClick = () => {
     navigate("/recipes/new");
   };
 
-  const handleCategory = (category) => {
-    setCategory(category);
+  const handleCategory = (selectedCategory) => {
+    setCategory(selectedCategory);
   };
 
-  const handleSearch = (e)=>{
+  const handleSearch = (e) => {
     const word = e.target.value;
     setSearchWord(word);
-
-    console.log(word);
-
-  }
+  };
 
   const categories = [
     "All",
@@ -58,62 +56,48 @@ export default function RecipeList() {
   ];
 
   return (
-    <>
-      <div className="container-fluid">
-        <div className="row">
-          <aside className={`col-md-3 d-none d-md-block`}>
-            <div className="p-3">
-              <h2 className="text-center">Categories</h2>
-              <ul className="list-group list-group-flush">
-                {categories.map((cate) => (
-                  <li
-                    key={cate}
-                    className={`list-group-item ${
-                      category === cate ? "active" : ""
-                    }`}
-                    onClick={() => handleCategory(cate)}
-                    style={{ cursor: "pointer" }}
-                  >
-                    {cate}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </aside>
+    <div className="container-fluid">
+      <div className="row">
+        <aside className="col-md-3 d-none d-md-block">
+          <div className="p-3">
+            <h2 className="text-center">Categories</h2>
+            <ul className="list-group list-group-flush">
+              {categories.map((cate) => (
+                <li
+                  key={cate}
+                  className={`list-group-item ${category === cate ? "active" : ""}`}
+                  onClick={() => handleCategory(cate)}
+                  style={{ cursor: "pointer" }}
+                >
+                  {cate}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </aside>
 
-          <main className="col-md-9">
-            <div className="d-flex justify-content align-items-center mb-4">
-              <h1 className="mx-auto">Recipes</h1>
-            </div>
-            <input
-                type="text"
-                placeholder="Search..."
-                style={{width : "100%"}}
-                value={searchWord}
-                onChange={handleSearch}
-            />
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={handleClick}
-              style={{ width: '30%' }}
-            >
-              ADD
-            </button>
-            <AllRecipes category = {category} PropsRecipes={recipes} searchWord = {searchWord}/>
-              <button
-                type="button"
-                className="btn btn-primary"
-                onClick={handleClick}
-                style={{ width: '10%' }}
-              >
-                ADD
-              </button>
-            </div>
-            <AllRecipes category = {category} PropsRecipes={recipes}/>
-          </main>
-        </div>
+        <main className="col-md-9">
+          <div className="d-flex justify-content align-items-center mb-4">
+            <h1 className="mx-auto">Recipes</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Search..."
+            style={{ width: "100%" }}
+            value={searchWord}
+            onChange={handleSearch}
+          />
+          <button
+            type="button"
+            className="btn btn-primary my-3"
+            onClick={handleClick}
+            style={{ width: "30%" }}
+          >
+            ADD
+          </button>
+          <AllRecipes category={category} PropsRecipes={recipes} searchWord={searchWord} />
+        </main>
       </div>
-    </>
+    </div>
   );
 }
