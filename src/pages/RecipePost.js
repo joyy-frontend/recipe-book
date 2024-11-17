711566a76216c57ded9e790c0b36efdee580ddf3
rhkk7157 <rhkk7157@gmail.com>
2024-11-16 16:28:14 -0800

    made separate detail page, edit page

--- src/pages/RecipePost.js
+++ src/pages/RecipePost.js
@@ -8,8 +8,11 @@ export default function RecipePost() {
         "salad", "dessert", "vegetarian", "soup", "seafood"
     ];
     const navigate = useNavigate();
+    const [isEditStatus, setIsEditStatus] = useState(false);
+    const [recipeUser, setRecipeUser] = useState('');
     const [date, setDate] = useState('');
     const { recipeId } = useParams();
+    const [currentUser, setCurrentUser] = useState('');
     const [recipe, setRecipe] = useState({ 
         id: 0,
         title: '', 
@@ -19,13 +22,13 @@ export default function RecipePost() {
         image: '',
         likes: 0 
     });
+    
     const handleSubmit = (e) => {
         e.preventDefault();
         let storageData = JSON.parse(localStorage.getItem("recipe")) || [];
-        if(recipeId) {
+        if (recipeId) {
             const index = storageData.findIndex(item => item.id === parseInt(recipeId, 10));
-
-            if(index !== -1) {
+            if (index !== -1) {
                 storageData[index] = { ...recipe, id: parseInt(recipeId, 10)};
                 localStorage.setItem("recipe", JSON.stringify(storageData));
                 alert("Recipe updated Successfully");
@@ -34,15 +37,11 @@ export default function RecipePost() {
                 alert("Recipe not found for editing");
             }
         } else {
-            const newData = {
-                ...recipe,
-                id: storageData.length + 1,
-                likes: 0
-            }
+            const newData = { ...recipe, id: storageData.length + 1, likes: 0 };
             storageData.push(newData);
             localStorage.setItem("recipe", JSON.stringify(storageData));
             alert("Recipe saved successfully");
-            navigate('/recipes')
+            navigate('/recipes');
         }
     }
 
@@ -78,59 +77,76 @@ export default function RecipePost() {
             }
         });
     };
-    useEffect(() => {
-        if(recipeId) {
-            const storageData = JSON.parse(localStorage.getItem('recipe')) || [];
-            const currentData = storageData.find(item => item.id === parseInt(recipeId));
-            if(currentData) {
-                setRecipe(currentData);
-            }
-        }
-    }, [recipeId]);
+
 
     useEffect(() => {
-        const user = JSON.parse(localStorage.getItem("user"));
-        if(user) {
-            setRecipe(prev => ({
-                ...prev, user: user.email
-            }))
-        } else {
-            alert("Please Login first");
-            navigate(-1);
+    if (recipeId) {
+        const storageData = JSON.parse(localStorage.getItem('recipe')) || [];
+        const currentData = storageData.find(item => item.id === parseInt(recipeId));
+        if (currentData) {
+            setRecipe(currentData);
         }
-        
-        const today = new Date().toISOString().split("T")[0];
-        setDate(today);    
-    }, []);
+    }
+}, [recipeId]);
+
+useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    const recipeUser = JSON.parse(localStorage.getItem('recipe'));
+    if(recipeUser) {
+        setRecipeUser(recipeUser[recipeId-1].user);
+    }
+    if (user) {
+        setRecipe((prev) => ({ ...prev, user: user.email }));
+        setCurrentUser(user);
+       
+    } else {
+        alert("Please Login first");
+        navigate(-1);
+    }
+    
+    const today = new Date().toISOString().split("T")[0];
+    setDate(today);
+}, [recipeId]);
+
+useEffect(() => {
+    if (recipeUser && currentUser) {
+        if (recipeUser === currentUser.email) {
+            setIsEditStatus(true);
+        } else {
+            setIsEditStatus(false);
+        }
+    }
+}, [recipeUser, currentUser]);
 
     return (
         <div className="container mt-5">
             <h1 className="text-center mb-4">{recipeId ? 'Recipe Detail' : 'Recipe Post'}</h1>
             <form className="form-container" onSubmit={handleSubmit}>
                 <div className="mb-3">
-                    <label for="title" className="form-label">Title</label>
+                    <label htmlFor="title" className="form-label">Title</label>
                     <input 
                         type="text" 
                         className="form-control" 
-                        id="title" 
+                        id="title"
                         onChange={(e) => setRecipe({...recipe, title: e.target.value })} 
                         value={recipe.title}
                         required 
+                        readOnly={!isEditStatus} 
                     />
                 </div>
                 <div className="mb-3">
-                    <label for="user" className="form-label">User</label>
+                    <label htmlFor="user" className="form-label">User</label>
                     <input 
                         type="text" 
                         className="form-control" 
                         id="user" 
                         onChange={(e) => setRecipe({...recipe, user: e.target.value })} 
                         value={recipe.user} 
-                        readOnly
+                        readOnly 
                     />
                 </div>
                 <div className="mb-3">
-                    <label for="content" className="form-label">Content</label>
+                    <label htmlFor="content" className="form-label">Content</label>
                     <textarea 
                         className="form-control" 
                         id="content" 
@@ -138,40 +154,48 @@ export default function RecipePost() {
                         onChange={(e) => setRecipe({...recipe, content: e.target.value })} 
                         value={recipe.content}  
                         required
+                        readOnly={!isEditStatus} 
                     ></textarea>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="category" className="form-label">Category</label>
                     <br />
-                    <div class="btn-group-category" role="group" aria-label="Category Selector">
+                    <div className="btn-group-category" role="group" aria-label="Category Selector">
                         {categories.map((category) => (
                             <button
                                 key={category}
                                 type="button"
                                 className={`btn ${recipe.category.split(',').map(item => item.trim()).includes(category) ? 'btn-primary' : 'btn-outline-primary'}`}
                                 onClick={() => handleCategoryClick(category)}
-                                >
+                                disabled={!isEditStatus} 
+                            >
                                 {category.charAt(0).toUpperCase() + category.slice(1)}
                             </button>
                         ))}
                     </div>  
                 </div>
                 <div className="mb-3">
-                    <label for="date" className="form-label">Date</label>
-                    <input type="date" className="form-control" id="date" value={date} readOnly/>
+                    <label htmlFor="date" className="form-label">Date</label>
+                    <input type="date" className="form-control" id="date" value={date} readOnly />
                 </div>
-                <div className="mb-3">
-                    <label for="img" className="form-label">Image</label>
-                    <input type="file" className="form-control" id="img" accept="image/*" onChange={handleImageChange} style={{ border: '1px solid red'}}/>
-                </div>
+             
+                    <div className="mb-3">
+                        <label htmlFor="img" className="form-label">Image</label>
+                    {
+                        isEditStatus &&
+                        <input type="file" className="form-control" id="img" accept="image/*" onChange={handleImageChange} disabled={!isEditStatus} />
+                    }
+                    </div>
+             
+                {recipe.image && (
+                    <div className="mb-3">
+                        <img src={recipe.image} alt="Preview" style={{ width: "300px", height: "300px" }} />
+                    </div>
+                )}
                 {
-                    recipe.image && (
-                        <div className="mb-3">
-                            <img src={recipe.image} alt="Preview" style={{ width: "300px", height: "300px" }} />
-                        </div>
-                    )
+                    isEditStatus &&
+                    <button type="submit" className="btn btn-primary">Update Recipe</button>
                 }
-                <button type="submit" className="btn btn-primary">{recipeId ? "Update Recipe" : "Create Recipe"}</button>
             </form>
         </div>
     );
