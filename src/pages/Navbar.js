a9585a35fce3c821776488df03b76ff3f46919e7
minimanimox <purplemushroomsoup@gmail.com>
2024-11-18 16:30:25 -0800

    feat: nav link active

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink  } from "react-router-dom";
 import "../Custom.css";
 import logo from "../assets/images/logo.png";
 import { useEffect, useState } from "react";
@@ -14,72 +14,93 @@ export default function Navbar({ isLoggedIn, onLogout }) {
       console.log(currentUser);
     }
   }, [])
-  return (
-    <nav className="navbar navbar-expand-sm navbar-light">
-      <div className="container">
-      <div className="navbar-header">  {/* logo and hamburger */}
-          <Link className="navbar-brand" to="/">
-            <img src={logo} alt="logo" className="nav-logo" />
-          </Link>
-          <button 
-            className="navbar-toggler" 
-            type="button" 
-            data-bs-toggle="collapse" 
-            data-bs-target="#navbarNav"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-        </div>
-
-        {/* nav menu */}
-        <div className="collapse navbar-collapse" id="navbarNav">
-            {
-              isLoggedIn &&
-              <div className="nav-link" style={{ whiteSpace: 'nowrap', paddingTop: '20px'}}>
-                <p style={{ color: '#C86322' }}>{currentUser}, logged in!</p>
+    return (
+      <nav className="navbar navbar-expand-sm navbar-light">
+        <div className="container">
+          <div className="navbar-header">
+            <NavLink className="navbar-brand" to="/">
+              <img src={logo} alt="logo" className="nav-logo" />
+            </NavLink>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </div>
+  
+          <div className="collapse navbar-collapse" id="navbarNav">
+            {isLoggedIn && (
+              <div
+                className="nav-link"
+                style={{ whiteSpace: "nowrap", paddingTop: "20px" }}
+              >
+                <p style={{ color: "#C86322" }}>{currentUser}, logged in!</p>
               </div>
-            }
-          <ul className="navbar-nav ms-auto align-items-center">  {/* ms-auto로 오른쪽 정렬 */}
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/recipes">
-                Recipes
-              </Link>
-            </li>
-            {isLoggedIn ? (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/mypage">
-                    MyPage
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <button className="btn btn-link nav-link" onClick={onLogout}>
-                    Logout
-                  </button>
-                </li>
-              </>
-            ) : (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/login">
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/register">
-                    Register
-                  </Link>
-                </li>
-              </>
             )}
-          </ul>
+            <ul className="navbar-nav ms-auto align-items-center">
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  to="/"
+                  activeClassName="active"
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  to="/recipes"
+                  activeClassName="active"
+                >
+                  Recipes
+                </NavLink>
+              </li>
+              {isLoggedIn ? (
+                <>
+                  <li className="nav-item">
+                    <NavLink
+                      className="nav-link"
+                      to="/mypage"
+                      activeClassName="active"
+                    >
+                      MyPage
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <button className="btn btn-link nav-link" onClick={onLogout}>
+                      Logout
+                    </button>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <NavLink
+                      className="nav-link"
+                      to="/login"
+                      activeClassName="active"
+                    >
+                      Login
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink
+                      className="nav-link"
+                      to="/register"
+                      activeClassName="active"
+                    >
+                      Register
+                    </NavLink>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  );
+      </nav>
+    );
 }
